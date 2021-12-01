b87f227a01cf35d400ae4e053af86ee2dbc96534
Kåre von Geijer <kare.kvg@gmail.com>
2021-12-01 16:59:40 +0100

    Finished part 2.
    Basically exactly the same as part one but now first creates the
    sliding sum iterator. Difficulty with creating parametrized
    function. Is there a better way than cloning and zipping iterators?

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,12 +1,41 @@
 use std::io::Read;
 
-
 fn get_input() -> String {    
     let mut file = std::fs::File::open("input1.in").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
     return contents;
+}
+
+
+fn part2() {
+    /* Just like part 1, but first construct sliding sum list */
+    let contents = get_input();
+
+    // Do everything in several steps to be more clear
+    let lines = contents.split("\n");
+    let depths = lines.map(|x| x.parse::<i32>().unwrap());
+
+    let sliding_zipped = depths.clone().skip(2).zip(depths.clone().skip(1)).zip(depths);
+    let sliding_sum = sliding_zipped.map(|((x, y), z)| x + y + z);
+    
+    let increases = count_increases(sliding_sum);
+
+    println!("Number of depth increases for sliding sum: {}", increases);
+}
+
+
+// The type of this one is confusing. I need to learn traits better
+fn count_increases<I: Clone>(list: I) -> usize 
+where 
+    I: Iterator<Item = i32>
+{
+    let zipped = list.clone().skip(1).zip(list);
+    let diffs = zipped.map(|(x, y)| x - y);
+    let increases = diffs.filter(|x| *x > 0).count();
+
+    return increases;
 }
 
 
@@ -18,9 +47,7 @@ fn part1_2() {
     let lines = contents.split("\n");
     let depths = lines.map(|x| x.parse::<i32>().unwrap());
 
-    let zipped = depths.clone().skip(1).zip(depths);
-    let diffs = zipped.map(|(x, y)| x - y);
-    let increases = diffs.filter(|x| *x > 0).count();
+    let increases = count_increases(depths);
 
     println!("Number of depth increases: {}", increases);
 
@@ -28,7 +55,7 @@ fn part1_2() {
 
 
 fn part1_1() {
-    /* The main crux here is how to read and parse input well */
+    // Naive first solution, maybe look at part1_2 instead.
     let contents = get_input();
 
     let lines = contents.split("\n");
@@ -51,4 +78,5 @@ fn part1_1() {
 
 fn main() {
     part1_2();
+    part2();
 }
