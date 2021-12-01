b0696a7748087e88b739c0515237cf57f318d464
Kåre von Geijer <kare.kvg@gmail.com>
2021-12-01 16:26:47 +0100

    Made version of part one with ~zipwith

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,19 +1,43 @@
 use std::io::Read;
 
-fn day1 () {
-    /* The main crux here is how to read and parse input well */
 
+fn get_input() -> String {    
     let mut file = std::fs::File::open("input1.in").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
-    let mut lines = contents.split("\n");
+    return contents;
+}
 
-    let mut last = lines.next().unwrap();
+
+fn part1_2() {
+    /* Now try to do without for loop */
+    let contents = get_input();
+
+    // Do everything in several steps to be more clear
+    let lines = contents.split("\n");
+    let depths = lines.map(|x| x.parse::<i32>().unwrap());
+
+    let zipped = depths.clone().skip(1).zip(depths);
+    let diffs = zipped.map(|(x, y)| x - y);
+    let increases = diffs.filter(|x| *x > 0).count();
+
+    println!("Number of depth increases: {}", increases);
+
+}
+
+
+fn part1_1() {
+    /* The main crux here is how to read and parse input well */
+    let contents = get_input();
+
+    let lines = contents.split("\n");
+    let mut depths = lines.map(|x| x.parse::<i32>().unwrap());
+
+    let mut last = depths.next().unwrap();
     let mut diffs = 0;
-    for line in lines {
-        let depth:i32 = line.parse().unwrap();
 
+    for depth in depths {
         if depth > last {
             diffs += 1;
         }
@@ -24,6 +48,7 @@ fn day1 () {
 
 }
 
+
 fn main() {
-    day1();
+    part1_2();
 }
