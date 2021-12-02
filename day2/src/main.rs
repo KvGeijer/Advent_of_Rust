b8719693ea6c0f27618f09c5b723049bf6388379
Kåre von Geijer <kare.kvg@gmail.com>
2021-12-02 08:06:07 +0100

    Finished day 2.
    I switched to BufReader and .lines() for reading the input. Also
    used regex for the first time in Rust. Was quite nice but as usual
    a bit annoying with all error handling.

---
+++ day2/src/main.rs
@@ -0,0 +1,81 @@
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+use regex::Regex;
+
+
+struct Position {
+    depth: i32,
+    dist: i32,
+    aim: i32,
+}
+
+// To be able to print it as a normal value. Not really needed.
+impl std::fmt::Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "(dist: {}, depth: {}, aim {})", self.dist, self.depth, self.aim)
+    }
+}
+
+
+fn get_lines(path: &str) -> std::io::Lines<BufReader<File>> {    
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+
+    buffered.lines()
+}
+
+fn part2() {
+    let some_lines = get_lines("input1.in").map(|line| line.unwrap());
+    let mut pos = Position{dist: 0, depth: 0, aim: 0};
+
+    let re = Regex::new(r"^(\w+) (\d+)$").unwrap();
+    for line in some_lines {
+        let caps = re.captures(&line).unwrap();
+
+        let command = caps.get(1).unwrap().as_str();
+        let nbr: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        match command {
+            "down" => pos.aim += nbr,
+            "up" => pos.aim -= nbr,
+            "forward" => {
+                pos.dist += nbr;
+                pos.depth += nbr * pos.aim;
+            }
+            _ => println!("Strange match: {:?}", command),
+        }
+
+    }
+
+    println!("Final position using aim: {}, mult = {}", pos, pos.depth * pos.dist);
+}
+
+
+fn part1() {
+    let some_lines = get_lines("input1.in").map(|line| line.unwrap());
+    let mut pos = Position{dist: 0, depth: 0, aim: 0};
+
+    let re = Regex::new(r"^(\w+) (\d+)$").unwrap();
+    for line in some_lines {
+        let caps = re.captures(&line).unwrap();
+
+        let command = caps.get(1).unwrap().as_str();
+        let nbr: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        match command {
+            "forward" => pos.dist += nbr,
+            "down" => pos.depth += nbr,
+            "up" => pos.depth -= nbr,
+            _ => println!("Strange match: {:?}", command),
+        }
+
+    }
+
+    println!("Final position: {}, mult = {}", pos, pos.depth * pos.dist);
+}
+
+
+fn main() {
+    part1();
+    part2();
+}
