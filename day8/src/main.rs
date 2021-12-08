4718239f299bf20e81d9a277d8c7232382a93868
Kåre von Geijer <kare.kvg@gmail.com>
2021-12-08 10:01:07 +0100

    Finished part 1.
    Only hard part was parsing. Got stuck on trying to transfer
    ownershit out of a vector. In the end found out you can't do it with
    indexing but instead have to use pop or remove methods.

---
+++ day8/src/main.rs
@@ -0,0 +1,65 @@
+use std::fs;
+
+
+struct Display {
+    input: [Vec<u8>; 10],
+    output: [Vec<u8>; 4],
+}
+
+
+impl Display {
+    fn parse (line: &str) -> Display{
+
+        let mut patterns: Vec<Vec<Vec<u8>>> = line.split(" | ")
+            .map(|half| half.split(' ')
+                .map(|pattern| pattern.as_bytes()
+                    .iter()
+                    .copied()
+                    .collect())
+                .collect())
+            .collect();
+
+        let mut disp = Display {input: Default::default(), output: Default::default()};
+        let mut output = patterns.pop().unwrap();
+        let mut input = patterns.pop().unwrap();
+
+        for i in (0..10).rev() {
+            disp.input[i] = input.pop().unwrap();
+        }
+
+        
+        for i in (0..4).rev() {
+            disp.output[i] = output.pop().unwrap();
+        }
+
+        disp
+    }
+}
+
+
+fn parse(path: &str) -> Vec<Display> {
+    let input = fs::read_to_string(path)
+        .expect("No file found");
+
+    input.split('\n')
+        .map(Display::parse) 
+        .collect()
+}
+
+
+fn part1(displays: &Vec<Display>) {
+    let count = displays.iter()
+        .map(|disp| disp.output
+            .iter()
+            .map(|pattern| [2, 3, 4, 7].contains(&pattern.len()) as u32)
+            .sum::<u32>())
+        .sum::<u32>();
+    
+    println!("The numbers 1, 7, 4 and 8 occured {} times!", count);
+}
+
+
+fn main() {
+    let displays = parse("input.in");
+    part1(&displays);
+}
