534f85bd356ad22236439efce066a389c3c558a0
Kåre von Geijer <kare.kvg@gmail.com>
2021-12-14 08:55:44 +0100

    Finished part 2.
    Has to completely rebuild my code to make it handle larger bigger
    numbers better. IN general I am unhappy with my handling of borrowed
    values. It feels like I should use borrowed values much more as keys
    in the hash maps.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 
 
-fn parse(path: &str) -> (Vec<u8>, HashMap<[u8; 2], u8>){
+fn parse(path: &str) -> (HashMap<[u8; 2], usize>, HashMap<[u8; 2], u8>, HashMap<u8, usize>){
     let input = std::fs::read_to_string(path).expect("Expected file");
     let (string, instr) = input.split_once("\n\n").unwrap();
 
-    let map = HashMap::from_iter(instr.split('\n')
+    let instr_map = HashMap::from_iter(instr.split('\n')
         .map(|line| {
             let (from, to) = line.split_once(" -> ").unwrap();
             let mut from_iter = from.as_bytes()
@@ -16,63 +16,60 @@ fn parse(path: &str) -> (Vec<u8>, HashMap<[u8; 2], u8>){
             (from_arr, to.as_bytes().iter().copied().next().unwrap())
         }));
 
-    // Could also have HashMap<[u8; 2], usize> keeping count of each pair. Probably more efficient
-    let work = string.as_bytes()
-        .iter()
-        .copied()
-        .collect();
+    let mut char_counts: HashMap<u8, usize> = HashMap::new();
 
-    (work, map)
+    let mut count_map = HashMap::from_iter(instr_map.keys().map(|&key| (key, 0)));
+
+    let mut chars = string.as_bytes().iter();
+    let mut first = chars.next().unwrap();
+
+    let cc = char_counts.entry(*first).or_insert(0);
+    *cc += 1;
+
+    while let Some(second) = chars.next() {
+        let count = count_map.get_mut(&[*first, *second]).unwrap();
+        *count += 1;
+
+        let cc = char_counts.entry(*second).or_insert(0);
+        *cc += 1;
+
+        first = second;
+    }
+
+    (count_map, instr_map, char_counts)
 }
 
-// Not efficient at all :/
-fn part1(mut string: Vec<u8>, map: &HashMap<[u8; 2], u8>) {
 
+// Counts should really have borrowed arrays as keys
+fn simulate(mut counts: HashMap<[u8; 2], usize>, instr: &HashMap<[u8; 2], u8>, mut char_count: HashMap<u8, usize>, iterations: usize) {
     
-    for _ in 0..40 {
-        let mut new = vec![];
+    for _it in 0..iterations {
+        let mut new: HashMap<[u8; 2], usize> = HashMap::from_iter(counts.keys().map(|&key| (key, 0)));
 
-        //println!("String: {:?}", string.iter().map(|&c| c as char).collect::<Vec<char>>());
+        for (&[first, second], &val) in counts.iter().filter(|(_, &v)| v > 0) {
+            let mid = *instr.get(&[first, second]).expect("All pairs should have instructions");
 
-        let mut first = string.pop().unwrap();
-        new.push(first.clone());
+            let cc = char_count.entry(mid).or_insert(0);
+            *cc += val;
 
-        while let Some(second) = string.pop() {
-           
-            if let Some(mid) = map.get(&[second, first]) {  // Reverse order
-                new.push(*mid);
+            for arr in [[first, mid], [mid, second]] {
+                let count = new.get_mut(&arr).unwrap();
+                *count += val;
             }
-            new.push(second.clone());
-
-            first = second;
         }
 
-        println!("New length: {}", new.len());
-
-        string = new;
-        string.reverse();
-
+        counts = new;
     }
 
-    let mut quantities = HashMap::new();
-    for c in string.iter() {
-        let counter = quantities.entry(c).or_insert(0);
-        *counter += 1;
-    }
-
-    let mut values: Vec<i32> = quantities.into_values().collect();
+    let mut values: Vec<usize> = char_count.into_values().collect();
     values.sort();
-    println!("Values: {:?}", values);
-
     let answer = values[values.len()-1] - values[0];
 
-    //println!("String: {:?}", string);
-    println!("Diff after 10 iterations: {}", answer);
+    println!("Diff after {} iterations: {}", iterations, answer);
 }
 
 
 fn main() {
-    let (string, map) = parse("input.in");
-    
-    part1(string.clone(), &map);
+    let (arr_counts, instr, char_counts) = parse("input.in");
+    simulate(arr_counts, &instr, char_counts, 40);
 }
